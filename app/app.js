ec484d7a406ea15f99fae3ec7cff36cee402412f
Chris Tarasovs <christarasovs@chriss-imac.home>
2017-01-08 00:09:36 +0000

    removed heading content part

--- app/app.js
+++ app/app.js
@@ -1,11 +1,8 @@
 'use strict';
-
-
 var awp = angular.module('awp', []);
 
-
 awp.controller('awpCtrl', function($scope){
-//  $scope.message = 'asda';
+
 console.log('sss');
 
 $scope.typography = true;
@@ -14,7 +11,7 @@ $scope.menu = {};
 $scope.switchMenu = function(selectedMenu) {
 
 
-$scope.selectItem = selectedMenu;
+  $scope.selectItem = selectedMenu;
   console.log($scope.selectItem);
 
 
@@ -22,10 +19,16 @@ $scope.selectItem = selectedMenu;
     if(selectedMenu == 'typography'){
       console.log('typography is enabled');
       //Enable submenu
-      $scope.typographySubmenu = true;
+      $scope.typographyOptions = true;
+      $scope.typographyFonts = true;
       $scope.typographyBold = true;
       $scope.typographyColorOptions = true;
       $scope.typographyItalic = true;
+      $scope.typographyHeadingOptions = true;
+      $scope.typographyHeading  = false;
+      $scope.typographyLink = true;
+      $scope.typographyLinkURL = false;
+      $scope.typographyUppercaseLowecase = true;
       //$scope.typographyBtnStyleOptions = true;
       //$scope.typographyColorOptions
         //  $scope.typographyColorHex
@@ -33,16 +36,41 @@ $scope.selectItem = selectedMenu;
     // if typographyColorOptions is false than enable
     if(selectedMenu == 'typographyColorOptions'){
         console.log('typographyColorOptions  is enabled');
-
+        $scope.typographyFonts = false;
+        $scope.typographyBold = false;
+        $scope.typographyHeadingOptions = false;
+        $scope.typographyHeading = false;
         $scope.typographyColorHex = true;
         $scope.typographyItalic = false;
-        $scope.typographyBold = false;
+        $scope.typographyLink = false;
+        $scope.typographyLinkURL = false;
         $scope.typographyUppercaseLowecase = false;
 
 
     }
+    if(selectedMenu == 'typographyHeadingOptions'){
+        $scope.typographyFonts = false;
+        $scope.typographyBold = false;
+        $scope.typographyItalic = false;
+        $scope.typographyHeading = true;
+        $scope.typographyLink = false;
+        $scope.typographyLinkURL = false;
+        $scope.typographyUppercaseLowecase = false;
+        $scope.typographyColorOptions = false;
+    }
+    if(selectedMenu == 'typographyLink'){
+      $scope.typographyFonts = false;
+      $scope.typographyBold = false;
+      $scope.typographyHeadingOptions = false;
+      $scope.typographyHeading = false;
+      $scope.typographyColorHex = false;
+      $scope.typographyItalic = false;
+      $scope.typographyLink = true;
+      $scope.typographyLinkURL = true;
+      $scope.typographyUppercaseLowecase = false;
+      $scope.typographyColorOptions = false;
 
-
+    }
       $scope.menu[selectedMenu] = !$scope.menu[selectedMenu];
       console.log($scope.menu[selectedMenu]);
 };
@@ -61,6 +89,12 @@ $scope.selectItem = selectedMenu;
   // typography submenu
     //ng-show="typographySubmenu"
     //typographySubmenu  = true;
+    //ng-show="typographyBold"
+    //ng-show="typographyHeadingOptions"
+    //ng-show="typographyHeadingOptions"
+    //ng-show="typographyLink"
+    //ng-show="typographyLinkURL"
+
     //ng-show="typographyUppercaseLowecase"
     //ng-show="typographyColorOptions"
     //ng-show="typographyColorHex"
@@ -68,28 +102,5 @@ $scope.selectItem = selectedMenu;
     //ng-show="typographyBtns"
     //ng-show="typographyBtnResize"
 
-// should pass the click variable of the menu
-  $scope.activeMenu = function (menuvar){
-    // log the vairable
-    $scope.variable = menuvar;
-    console.log($scope.variable);
-
-    return $scope.menuvar  = true;
-
-    //$scope.menuVariable = activelist;
-
-    // console the vairable
-
-
-  // Get the variable name
-
-  // Set the variable submenu active if there is
-
-  //Set the setting active if there is
-
-
-
-    //$scope.activelist = true;
-  }
 
 });
