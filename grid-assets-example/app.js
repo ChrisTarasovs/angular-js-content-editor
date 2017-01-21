2e6b9ef725b753de374b180d28104c5c9e9deb7e
Chris Tarasovs <christarasovs@chriss-imac.home>
2017-01-21 18:33:29 +0000

    gird layout added to grid-test file

--- grid-assets-example/app.js
+++ grid-assets-example/app.js
@@ -3,7 +3,7 @@ var isLeftResizing = false,
     isRightResizing = false;
 
 $(function () {
-    var blueBlock = $('#blueBlock'),
+    var elementBlock = $('#elementBlock'),
         handleLeft = $('#blue-left'),
         handleRight = $("#blue-right"),
         redBar = $("#test-bar"),
@@ -17,152 +17,181 @@ $(function () {
         canvasWidth  = canvas.width();
 
     // console.log(canvasLeft,canvasWidth,redBarWidth,redBarHeight);
-    blueBlock.dragged = blueBlock.dragging = false;
-    blueBlock.tmp={};
+    elementBlock.dragged = elementBlock.dragging = false;
+    
+    
+     el ={};
+    
     // blueBlock.click(function(){
     //   if(!blueBlock.dragged){
     //     blueBlock.dragged =true;
     //     blueBlock.appendTo('#toBePlaced');
     //   }
     // });
-    blueBlock.on('mousedown',function(e){
-      blueBlock.tmp.left = e.clientX - $('#blueBlock').offset().left;
-      blueBlock.tmp.top = e.clientY - $('#blueBlock').offset().top;
-      blueBlock.tmp.width = blueBlock.width();
-      blueBlock.tmp.height = blueBlock.height();
-      blueBlock.dragging = true;
-      if(!blueBlock.dragged){
-        blueBlock.appendTo(document.body);
-        blueBlock.css({
-          'width' : blueBlock.tmp.width,
-          'height': blueBlock.tmp.height,
-          'margin-left' : e.clientX - blueBlock.tmp.left,
-          'margin-top' :e.clientY - blueBlock.tmp.top
-        });
+    
+    //This is when user selects from the list
+    function updateElementlist(e){
+        el.top = e.clientY - $(e.target).offset().top;
+        el.left = e.clientX - $(e.target).offset().left;
+        el.width = $(e.target).width();
+        el.height = $(e.target).height();
+        return [el.top,el.left,el.width,el.height];
+    }
+    
+    //This is when user selects from grid
+    function updateElement(e){
+        el.tmp.top = e.clientY - $(e.target.parentNode).offset().top;
+        el.tmp.left = e.clientX - $(e.target.parentNode).offset().left;
+        el.tmp.width = $(e.target.parentNode).width();
+        el.tmp.height = $(e.target.parentNode).height();
+    }
+    
+
+    
+    
+    
+elementBlock.on('mousedown',function(e){
+       // var el = e.target.nodeName;
+       // var elt = e.target.getAttribute('id');
+        
+     updateElementlist(e);
+         
+        
+     elementBlock.dragging = true;
+    
+     //element is let go
+     if(!elementBlock.dragged){
+             $(e.target).appendTo(document.body);
+            $(e.target).css({
+              'width' : el.width,
+              'height': el.height,
+              'margin-left' : e.clientX - el.left,
+              'margin-top' :e.clientY - el.top
+            });
       }
       else{
-        if(blueBlock.tmp.parentCreated){
-          blueBlock.tmp.cursorLeft = e.clientX - (canvasLeft + Number.parseInt(greenRow.css('margin-left')) + Number.parseInt(blueWrapper.css('margin-left')) + Number.parseInt(blueBlock.css('margin-left')) );
-          blueBlock.tmp.cursorTop = e.clientY - (Number.parseInt($('#toBePlaced').css('margin-top')) + Number.parseInt(greenRow.css('margin-top')) )
+        if(elementBlock.tmp.parentCreated){
+          elementBlock.tmp.cursorLeft = e.clientX - (canvasLeft + Number.parseInt(greenRow.css('margin-left')) + Number.parseInt(blueWrapper.css('margin-left')) + Number.parseInt(elementBlock.css('margin-left')) );
+          elementBlock.tmp.cursorTop = e.clientY - (Number.parseInt($('#toBePlaced').css('margin-top')) + Number.parseInt(greenRow.css('margin-top')) )
         }
         else{
-          blueBlock.tmp.cursorLeft = e.clientX - (canvasLeft + Number.parseInt(blueBlock.css('margin-left')) );
-          blueBlock.tmp.cursorTop = e.clientY - (Number.parseInt($('#toBePlaced').css('margin-top')) + Number.parseInt(blueBlock.css('margin-top')) )
+          elementBlock.tmp.cursorLeft = e.clientX - (canvasLeft + Number.parseInt(elementBlock.css('margin-left')) );
+          elementBlock.tmp.cursorTop = e.clientY - (Number.parseInt($('#toBePlaced').css('margin-top')) + Number.parseInt(elementBlock.css('margin-top')) )
         }
       }
     });
     handleLeft.change(function(){
       blueWrapper.css({
-        'margin-left': redBarWidth * Math.floor(Number.parseInt(blueBlock.css('margin-left'))/redBarWidth),
-        'width' : blueWrapper.tmp.rightCoordinates  - ( canvasLeft + redBarWidth * Math.floor(Number.parseInt(blueBlock.css('margin-left'))/redBarWidth))
+        'margin-left': redBarWidth * Math.floor(Number.parseInt(elementBlock.css('margin-left'))/redBarWidth),
+        'width' : blueWrapper.tmp.rightCoordinates  - ( canvasLeft + redBarWidth * Math.floor(Number.parseInt(elementBlock.css('margin-left'))/redBarWidth))
       });
-      blueBlock.css({
-        'margin-left': Number.parseInt(blueBlock.css('margin-left')) - Number.parseInt(blueWrapper.css('margin-left')),
-        'width': blueBlock.tmp.width
+      elementBlock.css({
+        'margin-left': Number.parseInt(elementBlock.css('margin-left')) - Number.parseInt(blueWrapper.css('margin-left')),
+        'width': elementBlock.tmp.width
       });
     });
     handleRight.change(function(){
       blueWrapper.css({
-        'width' : redBarWidth * Math.ceil((Number.parseInt(blueBlock.css('margin-left')) + blueBlock.width())/redBarWidth)
+        'width' : redBarWidth * Math.ceil((Number.parseInt(elementBlock.css('margin-left')) + elementBlock.width())/redBarWidth)
       });
     });
-    var blueBlockRight;
+    var elementBlockRight;
     handleLeft.on('mousedown',function(){
-      if(!blueBlock.tmp.parentCreated && blueBlock.dragged){
+      if(!elementBlock.tmp.parentCreated && elementBlock.dragged){
         $(document).trigger('createParents');
       }
       isLeftResizing = true;
-      blueBlockRight = canvasLeft + blueBlock.offset().left + blueBlock.width();
+      elementBlockRight = canvasLeft + elementBlock.offset().left + elementBlock.width();
     });
     handleRight.on('mousedown',function(){
-      if(!blueBlock.tmp.parentCreated){
+      if(!elementBlock.tmp.parentCreated){
         $(document).trigger('createParents');
       }
       isRightResizing = true;
-      blueBlock.tmp.leftCoordinates  = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(blueBlock.css('margin-left'));
+      elementBlock.tmp.leftCoordinates  = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(elementBlock.css('margin-left'));
     });
     $(document).on('mousemove',function(e){
-      if(blueBlock.dragging && !blueBlock.dragged){
-        blueBlock.css({
-          'margin-left': e.clientX - blueBlock.tmp.left,
-          'margin-top': e.clientY - blueBlock.tmp.top
+      if(elementBlock.dragging && !elementBlock.dragged){
+        elementBlock.css({
+          'margin-left': e.clientX - elementBlock.tmp.left,
+          'margin-top': e.clientY - elementBlock.tmp.top
         })
       }
-      if(blueBlock.dragging && blueBlock.dragged && !isLeftResizing && !isRightResizing){
-        if(blueBlock.tmp.parentCreated){
+      if(elementBlock.dragging && elementBlock.dragged && !isLeftResizing && !isRightResizing){
+        if(elementBlock.tmp.parentCreated){
           greenRow.css({
-            'margin-top': e.clientY - blueBlock.tmp.cursorTop
+            'margin-top': e.clientY - elementBlock.tmp.cursorTop
           });
-          if(e.clientX - blueBlock.tmp.cursorLeft>= canvasLeft && e.clientX - blueBlock.tmp.cursorLeft + blueBlock.width() < canvasLeft +canvasWidth ){
-            blueBlock.css({
-              'margin-left':e.clientX - (canvasLeft + blueBlock.tmp.cursorLeft)
+          if(e.clientX - elementBlock.tmp.cursorLeft>= canvasLeft && e.clientX - elementBlock.tmp.cursorLeft + elementBlock.width() < canvasLeft +canvasWidth ){
+            elementBlock.css({
+              'margin-left':e.clientX - (canvasLeft + elementBlock.tmp.cursorLeft)
             });
             handleLeft.change();
             handleRight.change();
           }
         }
         else{
-          blueBlock.css({
-            'margin-top': e.clientY - blueBlock.tmp.cursorTop
+          elementBlock.css({
+            'margin-top': e.clientY - elementBlock.tmp.cursorTop
           });
         }
       }
       if(isLeftResizing){
-        if(!!blueWrapper && canvasLeft <= e.clientX && e.clientX <= blueBlock.tmp.rightCoordinates ){
-          blueBlock.css({
+        if(!!blueWrapper && canvasLeft <= e.clientX && e.clientX <= elementBlock.tmp.rightCoordinates ){
+          elementBlock.css({
             'margin-left': e.clientX - canvasLeft,
-            'width': blueBlock.tmp.rightCoordinates  - e.clientX
+            'width': elementBlock.tmp.rightCoordinates  - e.clientX
           });
-          blueBlock.tmp.width = blueBlock.width();
+          elementBlock.tmp.width = elementBlock.width();
           handleLeft.change();
         }
       }
       if(isRightResizing){
-        if(!!blueWrapper && blueBlock.tmp.leftCoordinates  <=e.clientX && e.clientX <= canvasLeft + canvasWidth){
-          blueBlock.css({
-            'width': e.clientX - blueBlock.tmp.leftCoordinates
+        if(!!blueWrapper && elementBlock.tmp.leftCoordinates  <=e.clientX && e.clientX <= canvasLeft + canvasWidth){
+          elementBlock.css({
+            'width': e.clientX - elementBlock.tmp.leftCoordinates
           });
           handleRight.change();
         }
       }
     }).on('mouseup',function(e){
-      if(blueBlock.dragging && !blueBlock.dragged){
+      if(elementBlock.dragging && !elementBlock.dragged){
         if(canvas.offset().left<=e.clientX && e.clientX<=canvas.offset().left+ canvas.width() && canvas.offset().top <= e.clientY && e.clientY <= canvas.offset().top + canvas.height()){
-          blueBlock.appendTo('#toBePlaced');
-          blueBlock.dragged = true;
-          blueBlock.dragging = false;
-          blueBlock.css({
+          elementBlock.appendTo('#toBePlaced');
+          elementBlock.dragged = true;
+          elementBlock.dragging = false;
+          elementBlock.css({
             'margin-left': 0,
-            'margin-top': Number.parseInt(blueBlock.css('margin-top')) - canvas.offset().top,
+            'margin-top': Number.parseInt(elementBlock.css('margin-top')) - canvas.offset().top,
             'width':canvas.width()
           });
         }
       }
-      if(blueBlock.dragging && blueBlock.dragged){
-        blueBlock.dragging = false;
+      if(elementBlock.dragging && elementBlock.dragged){
+        elementBlock.dragging = false;
       }
       if(isLeftResizing){
         isLeftResizing = false;
         blueWrapper.tmp.leftCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left'));
-        blueBlock.tmp.leftCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(blueBlock.css('margin-left'));
+        elementBlock.tmp.leftCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(elementBlock.css('margin-left'));
       }
       if(isRightResizing){
         isRightResizing = false;
         blueWrapper.tmp.rightCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) + blueWrapper.width();
-        blueBlock.tmp.rightCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(blueBlock.css('margin-left')) + blueBlock.width();
+        elementBlock.tmp.rightCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(elementBlock.css('margin-left')) + elementBlock.width();
       }
     });
 
     $(document).on('createParents',function(){
-      blueBlock.wrap('<div id="greenRow"></div>');
-      blueBlock.wrap('<div id="blueWrapper"></div>');
+      elementBlock.wrap('<div id="greenRow"></div>');
+      elementBlock.wrap('<div id="blueWrapper"></div>');
       blueWrapper = $('#blueWrapper');
       greenRow = $('#greenRow');
       greenRow.css({
         'margin-left': 0,
-        'margin-top': Number.parseInt(blueBlock.css('margin-top')) - canvas.offset().top,
-        'width':blueBlock.width(),
-        'height':blueBlock.height(),
+        'margin-top': Number.parseInt(elementBlock.css('margin-top')) - canvas.offset().top,
+        'width':elementBlock.width(),
+        'height':elementBlock.height(),
         'background':'green',
         // 'opacity':0.2,
         'z-index':1
@@ -170,13 +199,13 @@ $(function () {
       blueWrapper.css({
         'margin-left':0,
         'margin-top':0,
-        'width':blueBlock.width(),
-        'height':blueBlock.height(),
+        'width':elementBlock.width(),
+        'height':elementBlock.height(),
         'background':'yellow',
         // 'opacity':0.5,
         'z-index':2
       });
-      blueBlock.css({
+      elementBlock.css({
         'margin-left':0,
         'margin-top':0,
         'top':0,
@@ -184,10 +213,10 @@ $(function () {
         'bottom':0
       })
       blueWrapper.tmp = {};
-      blueBlock.tmp.parentCreated =true;
+      elementBlock.tmp.parentCreated =true;
       blueWrapper.tmp.leftCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left'));
       blueWrapper.tmp.rightCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) + blueWrapper.width();
-      blueBlock.tmp.leftCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(blueBlock.css('margin-left'));
-      blueBlock.tmp.rightCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(blueBlock.css('margin-left')) + blueBlock.width();
+      elementBlock.tmp.leftCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(elementBlock.css('margin-left'));
+      elementBlock.tmp.rightCoordinates = canvasLeft + Number.parseInt(blueWrapper.css('margin-left')) +Number.parseInt(elementBlock.css('margin-left')) + elementBlock.width();
     });
 });
