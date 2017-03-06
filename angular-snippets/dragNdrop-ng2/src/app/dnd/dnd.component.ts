e4bf0a1b6857f30e899b4cbbb24e324aa89e480f
Chris Tarasovs <kristaps.tarasovs@gmail.com>
2017-03-06 00:14:03 +0000

    ng2 dnd version February

--- angular-snippets/dragNdrop-ng2/src/app/dnd/dnd.component.ts
+++ angular-snippets/dragNdrop-ng2/src/app/dnd/dnd.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { TextComponent } from './text/text.component';
 
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './dnd.component.html',
   styleUrls: ['./dnd.component.css']
 })
 export class DndComponent  {
+    dragOperation: Boolean = false;
    widgets: Array<any> = [];
    containers: Array<any> = [];
    
    constructor(){
-     this.containers.push(new Container([new Widget('sds')]));
+     this.containers.push(new Container(1, [new Widget('1')]));
+      this.containers.push(new Container(2, [new Widget('2')]));
     
      this.widgets.push(new Widget('Text a'));
-     this.widgets.push(new Widget('Text b'));
    }
    
    drop(item){
-   console.log(222);
     var target = item.mouseEvent.target,
         index;
     
@@ -41,17 +45,54 @@ export class DndComponent  {
     
    }
    
-   dragEnter(element){
+   onDropSuccess(widget: any, event: any) {
+    this.dragOperation = false;
+    console.log('onDropSuccess', widget, event);
+   }
+   
+   onDragStart(widget: any, event: any) {
+    console.log('onDragStart', widget, event);
+   }
+   
+   onDragEnter(widget: any, event: any) {
+    console.log('onDragEnter', widget, event);
+   }
+   
+   onDragSuccess(widget: any, event: any) {
+    console.log('onDragSuccess', widget, event);
+   }
+   
+   onDragOver(widget: any, event: any) {
+    console.log('onDragOver', widget, event);
+   }
+   
+   onDragEnd(widget: any, event: any) {
+    console.log('onDragOver', widget, event);
+   }
+onDragLeave(widget: any, event: any) {
+    console.log('onDragLeave', widget, event);
+   }
+   
+   
+   onMouseDown(){
+    this.dragOperation = true;
+    console.log('mouse down');
+   }
+   
+   onMouseUp(event: any){
+    console.log(event);
+    this.dragOperation = false;
    }
 }
 
 class Container {
-  constructor(public widgets: Array<Widget>) {}
+  constructor(public id: Number, public widgets: Array<Widget>) {}
 }
 
 class Widget {
   constructor(public name: string) {}
 }
+
 
 //Callbacks:
 //dnd-dragstart
