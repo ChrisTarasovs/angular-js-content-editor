fe1bfe236b0a401c26582321926c1c6c3cc28cea
Chris Tarasovs <kristaps.tarasovs@gmail.com>
2017-03-20 20:04:51 +0000

    removed all examples

--- Javascript-snippets/dragExample/grid-assets-example/theme.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-	console.log('ready');
-   var browserheight = $( window ).height();
-	$('.nav-sidebar').css({'height' : browserheight })
-	$('#canvas').css({'height' : browserheight })
-	$('#canvas .bar').css({'height' : browserheight })
-	
-});
